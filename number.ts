4b3ac425b198f31d79de111768a705f5acf27557
윤국현 <djaudwls7873@gmail.com>
2023-07-02 00:00:46 +0900

    type number

---
+++ number.ts
@@ -0,0 +1,13 @@
+let decimal: number = 6; // 10진수
+
+let hex: number = 0xf00d; // 16진수
+
+let binary: number = 0b1010; // 2진수
+
+let octal: number = 0o744; // 8진수
+
+let notANumber: number = NaN; // Not A Number
+
+let underscoreNum: number = 1_000_000;
+
+// 자바스크립트에서도 사용 가능하다.
